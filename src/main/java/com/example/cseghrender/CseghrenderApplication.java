4b173984589d796cf0a9a03258b707bfa7fcb93f
Sailaja5125 <sailajapuvala5125@gmail.com>
2026-03-06 09:38:04 +0530

    initial commit

--- src/main/java/com/example/cseghrender/CseghrenderApplication.java
+++ src/main/java/com/example/cseghrender/CseghrenderApplication.java
@@ -8,6 +8,7 @@ public class CseghrenderApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CseghrenderApplication.class, args);
+		System.out.println("Hello, World!");
 	}
 
 }
